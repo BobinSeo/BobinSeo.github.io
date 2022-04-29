4276da846d89e6ded438f13d6f35786fdd5ae271
GroDavid <104490077+GroDavid@users.noreply.github.com>
2022-04-29 09:22:43 +0200

    Update app.js

--- rps/app.js
+++ rps/app.js
@@ -8,31 +8,30 @@ function getRndInteger(min, max, choice) {
     //Number(n2 = 1)
     if (n1 == n2) {
         end = 2; // gleichstand
-        return end;
+
     }
     if (n1 == 1 && n2 == 2) {
         end = 3; // win
-        return end;
+
     }
     if (n1 == 1 && n2 == 3) {
-        end = 1; // loose
-        return end;
+        end = 1; // lose
+
     }
     if (n1 == 2 && n2 == 1) {
-        end = 1; // loose
-        return end;
+        end = 1; // lose
+
     }
     if (n1 == 2 && n2 == 3) {
         end = 3; // win
-        return end;
     }
     if (n1 == 3 && n2 == 1) {
         end = 3; // win
-        return end;
+
     }
     if (n1 == 3 && n2 == 2) {
-        end = 1; // loose
-        return end;
+        end = 1; // lose
+
     }
     console.log("end: " + end);
     if (n2 == 1) {
@@ -44,6 +43,10 @@ function getRndInteger(min, max, choice) {
     if (n2 == 3) {
         document.getElementById("RPS").src="scissors.png";
     }
+	
+	
+	
+    return end;
 }
 
 
